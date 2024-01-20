9bc18746966ef5ad409226c41423ca3263733adc
2w <hsp003636@gmail.com>
2024-01-20 07:14:09 -0500

    init

--- beacon-flood.c
+++ beacon-flood.c
@@ -5,9 +5,7 @@
 #include <string.h> // memcpy, memcmp, memset, strcat
 #include <ctype.h>  // isupper
 #include <stdlib.h> // exit
-#include <sys/ioctl.h> // isMonitorMode
-#include <net/if.h>
-#include <netinet/in.h>
+#include <stdbool.h> // bool type
 
 
 struct Beacon_Packet {
@@ -19,48 +17,13 @@ struct Beacon_Packet {
     uint16_t sequence_number;
 }; // beacon frame 24byte
 
-struct Radiotap_Frame{
+struct Radiotap_Frame {
     uint8_t header_revison;
     uint8_t header_pad;
     uint16_t header_length;
     uint32_t header_presentflag;
     uint8_t idontknow[3]; // wireshark check <not found>
 };
-int isMonitorMode(char *interfaceName) {
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-
-    if (sockfd == -1) {
-        perror("Socket creation error");
-        return -1;
-    }
-
-    struct ifreq ifr;
-    memset(&ifr, 0, sizeof(struct ifreq));
-    strncpy(ifr.ifr_name, interfaceName, IFNAMSIZ);
-
-    if (ioctl(sockfd, SIOCGIFFLAGS, &ifr) == -1) {
-        perror("IOCTL error");
-        close(sockfd);
-        return -1;
-    }
-
-    close(sockfd);
-
-    return (ifr.ifr_flags & IFF_UP) != 0;
-}
-
-int changeMode(char *interfaceName) {
-    printf("Executing command: gmon %s mon0\n", interfaceName);
-    char command[256];
-    snprintf(command, sizeof(command), "gmon %s mon0", interfaceName);
-    int ret = system(command);
-
-    if (ret != 0) {
-        fprintf(stderr, "Failed to execute gmon command\n");
-        return 1;
-    }
-    return 0;
-}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -69,22 +32,40 @@ int main(int argc, char *argv[]) {
     }
 
     char *interfaceName = argv[1];
-    char *ssidListFile = argv[2];
+    char *filename = argv[2];
 
-    int result = isMonitorMode(interfaceName);
+    // pcap open
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *pcap = pcap_open_live(interfaceName, BUFSIZ, 1, 1000, errbuf); // 무차별 모드
+    if (pcap == NULL) {
+        fprintf(stderr, "pcap_open_live(%s) return null - %s\n", interfaceName, errbuf);
+        return -1;
+    } // 실패시 중단
 
-    if (result == -1) {
-        fprintf(stderr, "Error checking monitor mode for interface %s\n", interfaceName);
+    FILE *fp;
+    fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        fprintf(stderr, "파일을 열 수 없습니다.\n");
         return 1;
     }
 
-    if (!result) {
-        if (changeMode(interfaceName) != 0) {
-            return 1; // 모니터 모드로 변경 실패 시 종료
+    char beacon_name[35];
+    struct Beacon_Packet packet;
+
+    // beacon-flooding을 위한 무한 루프
+    while (true) {
+        while ((fgets(beacon_name, sizeof(beacon_name), fp)) != 0) {
+            // fgets로 읽은 beacon_name에 대한 처리가 필요
+            // 예를 들어, printf("%s", beacon_name) 등으로 화면에 출력하거나
+            // Beacon_Packet 구조체에 데이터를 채워넣는 등의 작업이 필요
+
+            // Beacon_Packet 구조체 초기화 예시
+            memset(&packet, 0, sizeof(struct Beacon_Packet));
+            // packet 구조체에 데이터 채워넣는 작업이 필요
         }
     }
 
-    // TODO: 나머지 코드 추가
-
+    fclose(fp);
+    pcap_close(pcap); // 캡쳐 중단
     return 0;
 }
