d568f44c04683edc7aca5c5f19fe685b8dad960e
2w <hsp003636@gmail.com>
2024-01-20 22:10:25 -0500

    send fail...

--- beacon-flood.c
+++ beacon-flood.c
@@ -1,39 +1,57 @@
 #include <stdio.h>
-#include <stdint.h> // uint
-#include <pcap.h>   // pcap
-#include <unistd.h> // sleep
-#include <string.h> // memcpy, memcmp, memset, strcat
-#include <ctype.h>  // isupper
-#include <stdlib.h> // exit
-#include <stdbool.h> // bool type
-
-
+#include <stdint.h>
+#include <pcap.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 struct Radiotap_Frame {
-    uint8_t header_revison;
+    uint8_t header_revision;
     uint8_t header_pad;
     uint16_t header_length;
     uint32_t header_presentflag;
-    uint8_t idontknow[3]; // wireshark check <not found>
-};  // Radiotap_Frame은 패킷에서 맨 처음 24byte를 차지
+    uint8_t idontknow[3];
+};
 
 struct Beacon_Packet {
-    uint16_t type;               // 패킷 타입 (Beacon 프레임의 경우 0x0800)
-    uint16_t duration;           // 프레임의 전송 지속 시간
-    uint8_t destination_address[6];  // 수신기의 MAC 주소
-    uint8_t source_address[6];       // 송신기의 MAC 주소
-    uint8_t bssid[6];                // BSSID (Basic Service Set Identifier)
-    uint16_t sequence_number;        // 프레임의 일련 번호
+    uint16_t type;
+    uint16_t duration;
+    uint8_t destination_address[6];
+    uint8_t source_address[6];
+    uint8_t bssid[6];
+    uint16_t sequence_number;
     struct {
         uint8_t tag_number;
         uint8_t tag_length;
         uint8_t ssid[32];
     } tag_ssid;
-}; // beacon frame은 패킷에서 Radiotap다음으로 24byte를 차지함
+};
 
+void initPacket(struct Beacon_Packet *packet, const char *ssid) {
+    // Structure initialization function modified
+    memset(packet, 0, sizeof(struct Beacon_Packet));
 
-void initPacket(struct Beacon_Packet *packet){
-    memset(packet, 0,sizeof(struct Beacon_Packet));
+    // Common values setting
+    packet->type = 0x0800; // Beacon Frame type
+    packet->duration = 0;
+
+    // Set destination_address to broadcast address (FF:FF:FF:FF:FF:FF)
+    memset(packet->destination_address, 0xFF, 6);
+
+    // Set source_address (can be your device's MAC address)
+    memset(packet->source_address, 0, 6);
+
+    // Set BSSID
+    memset(packet->bssid, 0, 6);
+
+    // Set sequence_number
+    packet->sequence_number = 0;
+
+    // Set SSID
+    packet->tag_ssid.tag_number = 0; // Tag number for SSID
+    packet->tag_ssid.tag_length = strlen(ssid); // Length of SSID
+    strncpy((char *)packet->tag_ssid.ssid, ssid, sizeof(packet->tag_ssid.ssid) - 1);
 }
 
 int main(int argc, char *argv[]) {
@@ -45,13 +63,12 @@ int main(int argc, char *argv[]) {
     char *interfaceName = argv[1];
     char *filename = argv[2];
 
-    // pcap open
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t *pcap = pcap_open_live(interfaceName, BUFSIZ, 1, 1000, errbuf); // 무차별 모드로 캡쳐
+    pcap_t *pcap = pcap_open_live(interfaceName, BUFSIZ, 1, 1000, errbuf);
     if (pcap == NULL) {
         fprintf(stderr, "pcap_open_live(%s) return null - %s\n", interfaceName, errbuf);
         return -1;
-    } // 실패시 중단
+    }
 
     FILE *fp;
     fp = fopen(filename, "rb");
@@ -60,33 +77,36 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char beacon_name[34]; //ssid + 줄바꿈
+    char beacon_name[34];
     struct Beacon_Packet packet;
 
-    // beacon-flooding을 위한 무한 루프
     while (true) {
+        // Move to the beginning of the file when reaching the end
+        if (feof(fp)) {
+            fseek(fp, 0, SEEK_SET);
+        }
+
         while ((fgets(beacon_name, sizeof(beacon_name), fp)) != 0) {
-            // 파일에서 한줄을 읽어서 가져와 flooding할 beacon name을 설정.
-            
-            //1. 맨 끝 공백 제거
-            char *tmp = strtok(beacon_name,"\r\n\t");
+            // 파일에서 한 줄을 읽어와 flooding할 beacon name을 설정.
+            char *ssid = strtok(beacon_name, "\r\n\t");
+            printf("ssid name : %s\n", ssid);
 
-            // Beacon_Packet 구조체 초기화 예시
-            memset(&packet, 0, sizeof(struct Beacon_Packet));
-            // packet 구조체에 데이터 채워넣는 작업이 필요
+            // Beacon_Packet 구조체 초기화 함수 호출
+            initPacket(&packet, ssid);
+            // Source Address변경
 
-            
-
-
-
-            // if (pcap_sendpacket(handle, (unsigned char*)&data, length) != 0){
-            //     printf("※  Beacon Flooding Fail..\n");
-            //     exit (-1);
-            // } // Beacon Flooding 패킷을 보냄
+            memcpy(packet.bssid,packet.source_address,6);
+            // 패킷을 pcap으로 전송
+            if (pcap_sendpacket(pcap, (unsigned char*)&packet, sizeof(packet)) != 0) {
+                printf("send fail\n");
+                exit(-1);
+            } // Beacon Flooding 패킷을 보냄
+            usleep(10000);
         }
     }
 
     fclose(fp);
-    pcap_close(pcap); // 캡쳐 중단
+    pcap_close(pcap);
+    printf("pcap close!\n");
     return 0;
 }
